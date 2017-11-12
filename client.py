435b8c8780d1fcff12fade92ce3a88e5b96150d2
Rumen Kolev <Rumen@rumens-macbook-pro-2.local>
2017-11-12 03:47:44 -0700

    Almost work

--- client.py
+++ client.py
@@ -1,5 +1,205 @@
 import sys
 import socket
+import hashlib
+import hmac
+import random
+import string
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+BLOCK_SIZE = 1024
+
+# Function used to encrypt every message sent subsequently after the first message from the client
+def encrypt_message(message):
+
+    global cipher
+    global cipher_function
+
+    if cipher == "aes128" or cipher == "aes256":
+        message = message.encode("UTF-8")
+
+        # Initialize padder
+        #padder = padding.PKCS7(128).padder()
+
+        # Initialize the encryptor
+        encryptor = cipher_function.encryptor()
+
+        # Pad the message
+        #padded_data = padder.update(message.encode("UTF-8"))
+        #padded_data += padder.finalize()
+
+        length = 16 - (len(message) % 16)
+        message += bytes([length]) * length
+
+        # Encrypt the padded message
+        encrypted_message = encryptor.update(message) + encryptor.finalize()
+
+        return encrypted_message
+
+    else:
+
+        # Null cipher so return message
+        return message.encode("UTF-8")
+
+# Similar as encrypt but for decryption
+def decrypt_message(message):
+
+    global cipher
+    global cipher_function
+
+    if cipher == "aes128" or cipher == "aes256":
+
+        # Initialize unpadder
+        #unpadder = padding.PKCS7(128).unpadder()
+
+        # Initialize decryptor
+        decryptor = cipher_function.decryptor()
+
+        decrypted_data = decryptor.update(message) + decryptor.finalize()
+        if message != b'':
+            decrypted_data = decrypted_data[:-decrypted_data[-1]]
+
+        #try:
+            # Unpad the decrypted data
+        #unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+
+        return decrypted_data.decode("UTF-8")
+
+            # Return decrypted if there is no padding to be unpadded
+        #except ValueError:
+            #unpadded_data = unpadder.update(decrypted_data)
+            #return decrypted_data.decode("UTF-8")
+
+    else:
+
+        # Null cipher so return message
+        return message.decode("UTF-8")
+
+
+def read_file(command, filename, client_socket):
+
+    try:
+        message = encrypt_message((command + ":" + filename))
+        client_socket.sendall(message)
+
+        # Receive server acknowledgement and file size
+        ack_size = client_socket.recv(BLOCK_SIZE)
+        ack_size = decrypt_message(ack_size)
+
+        ack_size = ack_size.split(":")
+
+        ack = ack_size[0]
+
+        print("Server response: " + ack)
+
+        # Check if file size received
+        if len(ack_size) == 2:
+
+            size_of_file = ack_size[1]
+            size_of_file_counter = int(size_of_file)
+
+
+            file_output = bytearray()
+
+            # Keep reading chunk data from the server and writing it to stdout
+            while size_of_file_counter > 0:
+
+                # We don't wanna read in 1024 if there is less than 1024 bytes left
+                if size_of_file_counter < BLOCK_SIZE:
+                    print("I want to die" + size_of_file + "counter " + str(size_of_file_counter))
+                    data_chunk = client_socket.recv(BLOCK_SIZE)
+                    data_chunk = decrypt_message(data_chunk)
+                    data_chunk = data_chunk.encode("UTF-8")
+
+                else:
+                    print("loop")
+                    data_chunk = client_socket.recv(BLOCK_SIZE)
+                    data_chunk = decrypt_message(data_chunk)
+                    data_chunk = data_chunk.encode("UTF-8")
+
+                # Add chunk to a byte array
+                file_output.extend(data_chunk)
+
+                # If nothing is read, break out of loop
+                if not data_chunk:
+                    break
+
+                # Decrease size counter for every 1024 bytes read
+                size_of_file_counter -= BLOCK_SIZE
+
+            # Write read data from server to standard output
+            sys.stdout.write(file_output.decode(encoding='UTF-8'))
+
+    except socket.error as e:
+
+        print("Server connection closing...")
+        quit()
+
+
+def write_file(command, filename, client_socket):
+    global cipher
+    try:
+
+        message = encrypt_message((command + ":" + filename))
+        client_socket.sendall(message)
+
+        # Receive server acknowledgement
+        ack = client_socket.recv(BLOCK_SIZE)
+        ack = decrypt_message(ack)
+        print("Server response: " + ack)
+
+        #content = sys.stdin.buffer.read(BLOCK_SIZE - 1).decode("UTF-8")
+        if cipher != "null":
+            content = sys.stdin.buffer.read(BLOCK_SIZE - 1).decode("UTF-8")
+            content = encrypt_message(content)
+        else:
+            content = sys.stdin.buffer.read(BLOCK_SIZE)
+
+
+
+
+        # While the content length read in from stdin is a 1023, keep getting chunk and sending it to the server
+        while len(content) == BLOCK_SIZE:
+            print("SENDING STUFF HERE \n")
+            client_socket.sendall(content)
+
+            if cipher != "null":
+                content = sys.stdin.buffer.read(BLOCK_SIZE - 1).decode("UTF-8")
+                content = encrypt_message(content)
+            else:
+                content = sys.stdin.buffer.read(BLOCK_SIZE)
+
+
+
+        # Send last block (if any) that is less than a 1024 bytes
+        client_socket.sendall(content)
+
+    except socket.error as e:
+
+        print("Server connection closing...")
+        quit()
+
+
+def challenge_response(secret_key, challenge):
+
+    # Encode the key into bytes
+    secret_key = secret_key.encode("UTF-8")
+
+    # Takes random challenge string (in bytes) concatenates it with secret key and gets the hash using sha256 hmac
+    hmac_hash = hmac.new(secret_key, challenge, digestmod=hashlib.sha256)
+
+    # Get the hash digest
+    hash_digest = hmac_hash.hexdigest()
+
+    return hash_digest
+
+
+# Generates a random string (used for generating the nonce)
+def string_generator():
+
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(16))
+
 
 if __name__ == "__main__":
 
@@ -20,6 +220,10 @@ if __name__ == "__main__":
         print("USAGE: 'python client.py [command] [filename] [hostname]:[port] [cipher] [key]'")
         quit()
 
+    if not (command == "read" or command == "write"):
+        print("Error: wrong operation given. Operation has to be either read or write")
+        quit()
+
     # Split the hostname and port number string argument into two variables holding each respectively
     hostport = hostport.split(":")
 
@@ -33,4 +237,78 @@ if __name__ == "__main__":
 
     client_socket.connect((hostname, port))
 
-    client_socket.sendall(cipher.encode("UTF-8"))
+    # Generate a nonce
+    nonce = string_generator()
+
+    # Set up the IV and session-key
+    init_vector = hashlib.sha256((key + nonce + "IV").encode("UTF-8"))
+
+    # IV has to be 16 bytes
+    init_vector = init_vector.hexdigest()
+    init_vector = init_vector[:16]
+
+    session_key = hashlib.sha256((key + nonce + "SK").encode("UTF-8"))
+
+    # If cipher used is aes128 we strip the session key to 16 bytes and pass that into the cipher function
+    if cipher == "aes128":
+
+        session_key = session_key.hexdigest()
+        session_key = session_key[:16]
+
+        # Set up the cipher
+        cipher_function = Cipher(algorithms.AES(session_key.encode("UTF-8")), modes.CBC(init_vector.encode("UTF-8")),
+                                 backend=default_backend())
+
+    # Otherwise if the cipher is aes256 we strip to 32 bytes
+    elif cipher == "aes256":
+
+        session_key = session_key.hexdigest()
+        session_key = session_key[:32]
+        cipher_function = Cipher(algorithms.AES(session_key.encode("UTF-8")), modes.CBC(init_vector.encode("UTF-8")),
+                                 backend=default_backend())
+
+    # Send the cipher and nonce to the server
+    client_socket.sendall((cipher + ":" + nonce).encode("UTF-8"))
+
+    # Receive server acknowledgement
+    ack = client_socket.recv(BLOCK_SIZE)
+    ack = decrypt_message(ack)
+    print("Server response: " + ack)
+
+    # Get the random string challenge from the server and create a response
+    challenge = client_socket.recv(BLOCK_SIZE)
+    challenge = challenge.strip()
+
+    challenge = decrypt_message(challenge)
+
+    print("CHALLENGE: " + challenge)
+
+    digest_response = challenge_response(key, challenge.encode("UTF-8"))
+    digest_response = encrypt_message(digest_response)
+
+    # Send the response back to the server
+    client_socket.sendall(digest_response)
+
+    # Receive authentication success or failure
+    authentication_response = client_socket.recv(BLOCK_SIZE)
+
+    authentication_response = decrypt_message(authentication_response)
+
+    print("Server authentication response: " + authentication_response)
+
+    # Send the filename and the operation we wish to do upon it to the server
+    if command == "write":
+
+        write_file(command, filename, client_socket)
+
+    else:
+
+        read_file(command, filename, client_socket)
+
+    final_message = client_socket.recv(BLOCK_SIZE)
+
+    final_message = decrypt_message(final_message)
+
+    print(final_message)
+
+    client_socket.close()
--- server.py
+++ server.py
@@ -1,5 +1,313 @@
 import sys
 import socket
+import hashlib
+import random
+import string
+import hmac
+import os
+import time
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+BLOCK_SIZE = 1024
+
+def request(client_socket):
+
+    # Receive the filename and operation
+    fileop = client_socket.recv(BLOCK_SIZE)
+
+    fileop = decrypt_message(fileop)
+    fileop = fileop.strip()
+
+
+    fileop = fileop.split(":")
+
+    operation = fileop[0]
+    filename = fileop[1]
+
+    # Check if operation and filename are valid
+    if operation == "read":
+
+        if not os.path.isfile(filename):
+
+            print("Error: file client is trying to read does not exist. Disconnecting client.")
+            message = encrypt_message(("Error, the file " + filename
+                                       + " you are trying to read does not exist. Disconnecting..."))
+            client_socket.sendall(message)
+            client_socket.close()
+
+        else:
+
+            # Send file-size to client
+            file_size = os.stat(filename).st_size
+            file_size = str(file_size)
+
+            message = encrypt_message(("Success, read operation proceeding." + ":" + file_size))
+            client_socket.sendall(message)
+
+            # Delay sending the file chunks so as to not send the above success, filesize along with
+            # a part of a file chunk
+            time.sleep(0.2)
+
+            data_exchange(client_socket, operation, filename)
+
+    elif operation == "write":
+
+        # Indicate success
+        message = encrypt_message("Success, write operation proceeding.")
+        client_socket.sendall(message)
+
+        time.sleep(0.2)
+
+        data_exchange(client_socket, operation, filename)
+
+    else:
+        # Operation does not exist
+        print("Operation request: " + operation+ " on file " + filename + " does not exist. Disconnecting client.")
+        message = encrypt_message(("Error, operation " + operation + " you are trying to perform on " + filename + " does not exist. Disconnecting..."))
+        client_socket.sendall(message)
+
+        client_socket.close()
+
+
+def data_exchange(client_socket, operation, filename):
+
+    if operation == "read":
+
+        # Open the file, read chunks, encrypt and send them to the client
+        file = open(filename, 'rb')
+        line = file.read(BLOCK_SIZE - 1)
+
+        while line:
+
+            message = encrypt_message(line.decode("UTF-8"))
+            client_socket.sendall(message)
+
+            # Read next line in file
+            line = file.read(BLOCK_SIZE - 1)
+
+        # Close file
+        file.close()
+        time.sleep(.1)
+        print("Operation successful. Disconnecting from client.")
+        message = encrypt_message((operation + " operation successful. Disconnecting..."))
+        client_socket.sendall(message)
+        client_socket.close()
+
+    # Otherwise we write to server from client side
+    else:
+
+        file_size = 0
+
+        # Get hard drive statistics (such as disk space) from current directory
+        stats = os.statvfs('/')
+
+        # disk size in bytes = (block size of file system * available number of blocks to the user) / 1024
+        # This is the disk size for the current directory
+        disk_size = (stats.f_frsize * stats.f_bavail) / BLOCK_SIZE
+
+        # We open file with filename and keep writing received chunks of data to it
+        with open(filename, 'wb') as file:
+
+            # Guarantees receive will never block indefinitely
+            #client_socket.setblocking(0)
+
+
+            write_chunk = client_socket.recv(BLOCK_SIZE)
+            decrypt_chunk = decrypt_message(write_chunk)
+
+            while len(write_chunk) == BLOCK_SIZE:
+
+                # If nothing is read, break out of the loop
+
+                print("WRITE CHUNK: " + decrypt_chunk)
+
+                chunk_length = len(write_chunk) #might be write, too tired
+
+                file_size += chunk_length
+
+                # Write to file on disk
+                if file_size < disk_size:
+                    print("122112312312312312313" + str(len(write_chunk)))
+                    file.write(decrypt_chunk.encode("UTF-8"))
+                    write_chunk = client_socket.recv(BLOCK_SIZE)
+                    decrypt_chunk = decrypt_message(write_chunk)
+                    print("HERERERERRERER " + str(len(write_chunk)))
+                # If the file we are reading in becomes larger
+                # than or equal to the available disk size, indicate error and disconnect
+                else:
+
+                    print("Error: client trying to write a file that is larger "
+                            "than the available disk size. Disconnecting client.")
+
+                    message = encrypt_message("Error, you are trying to upload a file that "
+                                                "is larger than the available server disk size. Disconnecting...")
+
+                    client_socket.sendall(message)
+
+                    client_socket.close()
+
+            if file_size < disk_size:
+                print("Last WRITE CHUNK: " + decrypt_chunk)
+
+                file.write(decrypt_chunk.encode("UTF-8"))
+
+
+            # If zero bytes being written, it will still create a zero
+            # bytes file but setblock will throw an exception on receive
+            # on receive
+            print("FUCING ERRRORSRSRSRSRSRSRSRSR")
+
+
+        file.close()
+        #can't send too fast to client
+        time.sleep(.1)
+        print("Operation successful. Disconnecting from client.")
+        message = encrypt_message((operation + " operation successful. Disconnecting..."))
+        client_socket.sendall(message)
+        client_socket.close()
+
+# Function used to encrypt every message sent subsequently after the first message to the client
+def encrypt_message(message):
+
+    global cipher
+    global cipher_function
+
+    if cipher == "aes128" or cipher == "aes256":
+
+        message = message.encode("UTF-8")
+
+        # Initialize padder
+        #padder = padding.PKCS7(128).padder()
+
+        # Initialize the encryptor
+        encryptor = cipher_function.encryptor()
+
+        length = 16 - (len(message) % 16)
+        message += bytes([length]) * length
+        print("kill me " + str(len(message)))
+        # Pad the message
+        #padded_data = padder.update(message.encode("UTF-8"))
+        #padded_data += padder.finalize()
+
+        # Encrypt the padded message
+        encrypted_message = encryptor.update(message) + encryptor.finalize()
+
+        return encrypted_message
+
+    else:
+
+        # Null cipher so return message
+        return message.encode("UTF-8")
+
+
+def decrypt_message(message):
+
+    global cipher
+    global cipher_function
+
+    if cipher == "aes128" or cipher == "aes256":
+
+        # Initialize unpadder
+        #unpadder = padding.PKCS7(128).unpadder()
+
+        # Initialize decryptor
+        decryptor = cipher_function.decryptor()
+
+        decrypted_data = decryptor.update(message) + decryptor.finalize()
+
+        if message != b'':
+            decrypted_data = decrypted_data[:-decrypted_data[-1]]
+
+        #return decrypted_data.decode("UTF-8")
+
+        #try:
+            # Unpad the decrypted data
+        #unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+
+        return decrypted_data.decode("UTF-8")
+
+        # Return decrypted if there is no padding to be unpadded
+        #except ValueError:
+            #unpadded_data = unpadder.update(decrypted_data)
+            #return decrypted_data.decode("UTF-8")
+
+
+
+    else:
+
+        # Null cipher so return message
+        return message.decode("UTF-8")
+
+
+# Generates a random string
+def string_generator():
+
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(32))
+
+
+def authentication(secret_key, client_socket):
+
+    authenticated = False
+
+    # Generate a 32 byte random string challenge
+    random_string = string_generator()
+
+    # Encrypt the random string message
+    encrypted_message = encrypt_message(random_string)
+
+    # Encode the secret key
+    secret_key = secret_key.encode("UTF-8")
+
+    # Delay sending the challenge by .2 seconds in the case of receiving the challenge with the previous success ack
+    time.sleep(0.2)
+
+    # Send the random string challenge to the client
+    client_socket.sendall(encrypted_message)
+
+    # Receive an encrypted SHA256 HMAC hexadecimal digest challenge response by the client
+    response = client_socket.recv(BLOCK_SIZE)
+
+    # Decrypt the response
+    response = decrypt_message(response)
+
+    # Strip of any special characters
+    response = response.strip()
+
+    print("RESPONSE: " + response)
+
+    # We hash the server secret key with the same random_string and obtain the hexadecimal digest
+    secret_key_hash = hmac.new(secret_key, msg=random_string.encode("UTF-8"), digestmod=hashlib.sha256)
+
+    # Obtain hex-digest
+    secret_key_digest = secret_key_hash.hexdigest()
+    print("SECRET KEY DIGEST: " + secret_key_digest)
+
+    # If the digest we computed is the same as the one provided by the response of the client, then the client has the
+    # correct secret key
+    if secret_key_digest == response:
+
+        # Send success to client
+        success_message = encrypt_message("Successfully authenticated")
+
+        client_socket.sendall(success_message)
+
+        # Client is now authenticated
+        authenticated = True
+
+        return authenticated
+
+    else:
+
+        failure_message = encrypt_message("Error: Wrong key. Disconnecting...")
+
+        client_socket.sendall(failure_message)
+
+        # Disconnect the client
+        client_socket.close()
+
+        return authenticated
 
 
 if __name__ == "__main__":
@@ -7,12 +315,15 @@ if __name__ == "__main__":
     HOST = "localhost"
 
     if len(sys.argv) != 3:
-        print("Wrong number of arguments provided\n")
+        print("Error: Wrong number of arguments provided\n")
         print("USAGE: 'python server.py [port] [key]'")
         quit()
 
     port = sys.argv[1]
     key = sys.argv[2]
+
+    print("Listening on port: " + port)
+    print("Using secret key: " + key)
 
     # Convert port string to int
     port = int(port)
@@ -30,8 +341,68 @@ if __name__ == "__main__":
 
         (client_socket, client_address) = server_socket.accept()
 
-        data = client_socket.recv(1024)
+        data = client_socket.recv(BLOCK_SIZE)
 
         data = data.decode("UTF-8")
 
-        print("DATA: " + data)
+        data = data.split(":")
+
+        if len(data) != 2:
+            print("Error: Cipher or nonce not provided. Disconnecting client.")
+
+            client_socket.sendall("Error: cipher or nonce not provided. Disconnecting...".encode("UTF-8"))
+            client_socket.close()
+        else:
+            cipher = data[0].lower()
+            nonce = data[1]
+
+            # Set up the IV and session-key
+            init_vector = hashlib.sha256((key + nonce + "IV").encode("UTF-8"))
+
+            # IV has to be 16 bytes
+            init_vector = init_vector.hexdigest()
+            init_vector = init_vector[:16]
+
+            session_key = hashlib.sha256((key + nonce + "SK").encode("UTF-8"))
+
+            print("CIPHER: " + cipher)
+            # Check if incorrect cipher was provided
+            if cipher != "aes128" and cipher != "aes256" and cipher != "null":
+                print("Error: Invalid cipher provided. Disconnecting client.")
+                client_socket.sendall(("Error " + cipher + " is not supported. Disconnecting...").encode("UTF-8"))
+                client_socket.close()
+
+            else:
+
+                # If cipher used is aes128 we strip the session key to 16 bytes and pass that into the cipher function
+                if cipher == "aes128":
+
+                    session_key = session_key.hexdigest()
+                    session_key = session_key[:16]
+
+                    # Set up the cipher
+                    cipher_function = Cipher(algorithms.AES(session_key.encode("UTF-8")), modes.CBC(init_vector.encode("UTF-8")),
+                                             backend=default_backend())
+
+                # Otherwise if the cipher is aes256 we strip to 32 bytes
+                elif cipher == "aes256":
+
+                    session_key = session_key.hexdigest()
+                    session_key = session_key[:32]
+                    cipher_function = Cipher(algorithms.AES(session_key.encode("UTF-8")), modes.CBC(init_vector.encode("UTF-8")),
+                                             backend=default_backend())
+
+                # Indicate success to user
+                success = encrypt_message("Success\n")
+                client_socket.sendall(success)
+
+                # Authenticate the client
+                authenticated = authentication(key, client_socket)
+
+                # If the client is authenticated, get the file request
+                if authenticated:
+
+                    request(client_socket)
+
+                else:
+                    print("Client was not authenticated. Secret keys not matching.")
